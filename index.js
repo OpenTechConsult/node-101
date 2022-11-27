6c2d79d3651ac8502fb54afe8d8d41a33444ca22
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-27 18:44:10 +0000

    - Read and Write Files
    use the fs.appendFile() function of the Node's FS core module to append data to a file.

--- index.js
+++ index.js
@@ -19,8 +19,18 @@ fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you', (er
     console.log('Write complete')
 })
 
+fs.appendFile(path.join(__dirname, 'files', 'test.txt'), 'Testing text', (err) => {
+    if (err) {
+        throw err
+    }
+    console.log('Append complete')
+})
+
+
+
 // exit on uncaught errors
 process.on('uncaughtException', err => {
     console.log(`There was an uncaught error: ${err}`)
     process.exit(1)
 })
+
