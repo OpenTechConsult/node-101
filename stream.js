853aa3f6d4ca81f33b4c3925336d131b6bf211c2
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-27 23:14:03 +0000

    Read and Write File
    Used the createReadStream and createWriteStream to perform file operations. Here we read from a file a copy what we read to another file using the stream.

---
+++ stream.js
@@ -0,0 +1,13 @@
+const { createReadStream, createWriteStream } = require('node:fs')
+const { join } = require('node:path')
+
+const filePath = join(__dirname, 'files', 'lorem.txt')
+const rs = createReadStream(filePath, {encoding: 'utf8'})
+
+const newFilePath = join(__dirname, 'files', 'newLorem.txt')
+const ws = createWriteStream(newFilePath)
+
+rs.on('data', (dataChunk) => {
+    ws.write(dataChunk)
+})
+
