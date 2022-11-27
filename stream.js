d65e976fd0188a1fd94ab56899df6e96904af66e
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-27 23:38:35 +0000

    Read and Write Files
    For more efficiency, we used the pipe function provided by the stream API

--- stream.js
+++ stream.js
@@ -2,12 +2,14 @@ const { createReadStream, createWriteStream } = require('node:fs')
 const { join } = require('node:path')
 
 const filePath = join(__dirname, 'files', 'lorem.txt')
-const rs = createReadStream(filePath, {encoding: 'utf8'})
+const rs = createReadStream(filePath, { encoding: 'utf8' })
 
 const newFilePath = join(__dirname, 'files', 'newLorem.txt')
 const ws = createWriteStream(newFilePath)
 
-rs.on('data', (dataChunk) => {
-    ws.write(dataChunk)
-})
+// rs.on('data', (dataChunk) => {
+//     ws.write(dataChunk)
+// })
+
+rs.pipe(ws)
 
