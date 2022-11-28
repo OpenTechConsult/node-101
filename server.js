8b59e19926370a6e75bddb10273ef627fc773d0e
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-28 16:28:02 +0000

    Build a Web Server
    created static contents destined to be serve by a HTTP web server
    change the package.json to recognize server.js as its main entry point.
    change the start and dev script to run server.js
    change de content of server.js to create an Http server that responds to the GET / and /index.html endpoint.

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('node:http')
+const path = require('node:path')
+const fs = require('node:fs')
+const fsPromises = require('node:fs/promises')
+const EventEmitter = require('node:events')
+
+// define the port on which the web server will listen to
+const PORT = process.env.PORT || 3500
+
+class Emitter extends EventEmitter { }
+
+// instantiate an emitter
+const myEmitter = new Emitter()
+
+// define the minimal server
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+
+    let filePath
+
+    if (req.url === '/' || req.url === 'index.html') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html')
+        filePath = path.join(__dirname, 'views', 'index.html')
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            res.end(data)
+        })
+    }
+})
+
+// launch the server by listening to the specified port
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
