989415746c2ef1ed59963c3d699ecd94b961340b
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-28 08:41:50 +0000

    NPM Modules
    initialized a node project
    installed third-party modules: date-fns and uuid
    installed nodemon as a dev dependency package
    created script to run the application: start and dev script
    tested the dependencies we've installed.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('testing!')
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+
+console.log(format(new Date(), 'yyyMMdd\tHH:mm:ss'))
+console.log('Hello')
+console.log(uuid())
