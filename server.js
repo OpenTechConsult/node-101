1e935afb11e3a7fed55210e3412ff7b8f9b7a58a
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-29 22:23:00 +0000

    Middleware
    installed the CORS package
    use the third party CORS middleware and whitelisted some addresses
    
    created custom error handler middleware and applied it at the end of the server.js file to log any error that will  occur

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const path = require('node:path')
 
+const cors = require('cors')
 const express = require('express')
 
 const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 
 const PORT = process.env.PORT || 3500
 
@@ -10,6 +12,20 @@ const app = express()
 
 //custom middleware logger
 app.use(logger)
+
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:5000', 'http://localhost:3500']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 // built-in middleware to handle urlencoded data
 // in other word, form data
@@ -62,8 +78,19 @@ const three = (req, res, next) => {
 
 app.get('/chain(.html)?', [one, two, three])
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.all('/*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" })
+
+    } else {
+        res.type('txt').send("404 Not Found")
+    }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
